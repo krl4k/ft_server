647bfa5b0ebe4003342576ae3a22c33595ab5c76
Foster Grisella <fgrisell@ev-r2.kzn.21-school.ru>
2021-01-24 12:28:31 +0300

    all work2

--- srcs/start.sh
+++ srcs/start.sh
@@ -1,12 +1,5 @@
 #!bin/bash
 
-##ssl 
-#openssl req -newkey rsa:2048 \
-#    -x509 -sha256 -days 365 -nodes \
-#    -out /etc/ssl/certs/example.crt \
-#    -keyout /etc/ssl/private/example.key\
-#    -subj "/C=RU/ST=Tatarstan/L=Kazan/O=Ecole/OU=21/CN=localhost"
-#
 service nginx start
 service mysql start
 service php7.3-fpm start
@@ -16,20 +9,12 @@ username='admin'
 password='admin'
 hostname='localhost'
 
-
 # WordPress database
 service mysql start
 mysql -e "CREATE DATABASE $db_name;"
 mysql -e "CREATE USER '$username'@'localhost' IDENTIFIED BY '$password';"
-#mysql  -e "CREATE USER 'adminwp'@'localhost' IDENTIFIED BY 'adminwp';"
 mysql -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$username'@'$hostname' WITH GRANT OPTION;"
 mysql -e "UPDATE mysql.user SET plugin='mysql_native_password' WHERE user='$username';"
 mysql -e "FLUSH PRIVILEGES;"
-
-
-#mysql -e "CREATE DATABASE wordpress;" -u root --skip-password
-#mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;" -u root --skip-password
-#mysql -e "FLUSH PRIVILEGES;" -u root --skip-password
-#mysql -e "UPDATE mysql.user SET plugin='' WHERE user='root';" -u root --skip-password
 
 bash
